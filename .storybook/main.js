d0e957df8d3570e9d9d71df896c5e56a7bd07656
AlexFrost-Head <afrosthead@boundinteractive.com>
2023-10-20 14:16:41 +1100

    Contrast added

---
+++ .storybook/main.js
@@ -0,0 +1,30 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+};
+module.exports = {
+  addons: ["storybook-contrast"]
+};
+module.exports = {
+  // ...
+  features: {
+    // ...
+    storyStoreV7: true
+  }
+};
+export default config;
