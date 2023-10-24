869311b60be12691957abb5979fc99295e155f8b
AlexFrost-Head <afrosthead@boundinteractive.com>
2023-10-24 17:53:47 +1100

    Trying to get this component to render

--- stories/Card.stories.ts
+++ stories/Card.stories.ts
@@ -2,7 +2,9 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Card } from './Card'
 
 const meta: Meta<typeof Card> = {
+    title: 'Example/Card',
     component: Card,
+    tags: ['autodocs'],
 };
 
 export default meta;
